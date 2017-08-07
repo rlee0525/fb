a8c38688b075f9cd42a17692be6d1adba36be427
Raymond Lee <raylee0525@gmail.com>
2017-08-07 14:16:54 -0700

    fb - 15

--- Algorithms/LeetCode/List.rb
+++ Algorithms/LeetCode/List.rb
@@ -138,6 +138,57 @@ def roman_to_int(s)
   result
 end
 
+# 15) 3 Sum
+# Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+# Note: The solution set must not contain duplicate triplets.
+# For example, given array S = [-1, 0, 1, 2, -1, -4],
+# A solution set is:
+# [
+#   [-1, 0, 1],
+#   [-1, -1, 2]
+# ]
+
+def three_sum(nums)
+  nums.sort!
+  solution = []
+
+  (0...nums.length).each do |idx|
+    if idx == 0 || nums[idx] != nums[idx - 1]
+      low = idx + 1
+      high = nums.length - 1
+
+      while low < high
+        if nums[idx] + nums[low] + nums[high] == 0
+          solution << [nums[idx], nums[low], nums[high]]
+          low += 1
+          high -= 1
+
+          low += 1 while nums[low] == nums[low - 1]
+          high -= 1 while nums[high] == nums[high + 1]
+        elsif nums[idx] + nums[low] + nums[high] > 0
+          high -= 1
+        else
+          low += 1
+        end
+      end
+    end
+  end
+
+  solution
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
