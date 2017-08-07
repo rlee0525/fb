5965c55daa1b755cf2aadaa4d4345a205c5e79fc
Raymond Lee <raylee0525@gmail.com>
2017-08-07 09:43:48 -0700

    update concepts

---
+++ Algorithms/Concepts.md
@@ -0,0 +1,5 @@
+# Basic Technical Concepts
+
+## Definitions
+- Atomicity (operations): Guarantee of isolation from interrupts, signals, concurrent processes and threads.
+- Atomicity (database): a property of database transactions which are guaranteed to either completely occur or have no effects.
--- Algorithms/LeetCode/List.rb
+++ Algorithms/LeetCode/List.rb
@@ -1,4 +1,6 @@
 # 1) Two Sum
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 def two_sum(nums, target)
   seen = {}
@@ -7,3 +9,28 @@ def two_sum(nums, target)
     seen[target - num] = idx
   end
 end
+
+# 10) Regex Matching
+# Implement regular expression matching with support for '.' and '*'.
+
+# '.' Matches any single character.
+# '*' Matches zero or more of the preceding element.
+
+# The matching should cover the entire input string (not partial).
+
+# The function prototype should be:
+# bool isMatch(const char *s, const char *p)
+
+# Some examples:
+# isMatch("aa","a") ? false
+# isMatch("aa","aa") ? true
+# isMatch("aaa","aa") ? false
+# isMatch("aa", "a*") ? true
+# isMatch("aa", ".*") ? true
+# isMatch("ab", ".*") ? true
+# isMatch("aab", "c*a*b") ? true
+
+def is_match(string, pattern)
+  
+end
+
