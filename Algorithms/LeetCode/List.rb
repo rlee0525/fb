bba45a6ea77ad7dd7eff907aca5fbccd2aa7225c
Raymond Lee <raylee0525@gmail.com>
2017-08-09 13:10:16 -0700

    fb - 38

--- Algorithms/LeetCode/List.rb
+++ Algorithms/LeetCode/List.rb
@@ -556,13 +556,13 @@ def search(nums, target)
     return mid if nums[mid] == target
 
     if nums[low] <= nums[mid]
-      if nums[low] <= target && target <= nums[mid]
+      if nums[low] <= target && target < nums[mid]
         high = mid - 1
       else
         low = mid + 1
       end
     else
-      if nums[mid] <= target && target <= nums[high]
+      if nums[mid] < target && target <= nums[high]
         low = mid + 1
       else
         high = mid - 1
@@ -573,6 +573,52 @@ def search(nums, target)
   -1
 end
 
+# 38) Count and Say
+# The count-and-say sequence is the sequence of integers with the first five terms as following:
+# 1.     1
+# 2.     11
+# 3.     21
+# 4.     1211
+# 5.     111221
+# 1 is read off as "one 1" or 11.
+# 11 is read off as "two 1s" or 21.
+# 21 is read off as "one 2, then one 1" or 1211.
+# Given an integer n, generate the nth term of the count-and-say sequence.
+# Note: Each term of the sequence of integers will be represented as a string.
+# Example 1:
+# Input: 1
+# Output: "1"
+# Example 2:
+# Input: 4
+# Output: "1211"
+
+def count_and_say(n)
+  output = current = "1"
+  
+  (n - 1).times do
+    output = ""
+    prev = current[0]
+    count = 0
+    idx = 0
+
+    while idx < current.length
+      if prev == current[idx]
+        count += 1
+      else
+        output += count.to_s + prev
+        prev = current[idx]
+        count = 1
+      end
+
+      idx += 1
+    end
+    
+    output += count.to_s + prev unless count == 0
+    current = output
+  end
+
+  current
+end
 
 
 
