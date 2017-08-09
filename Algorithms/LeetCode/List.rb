8ba5a914d1111b8a16573f813015d494167a1385
Raymond Lee <raylee0525@gmail.com>
2017-08-08 17:30:57 -0700

    fb - 26

--- Algorithms/LeetCode/List.rb
+++ Algorithms/LeetCode/List.rb
@@ -434,6 +434,30 @@ def reverse_k_group(head, k)
   dummy.next
 end
 
+# 26) Remove Duplicates from Sorted Array
+# Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+# Do not allocate extra space for another array, you must do this in place with constant memory.
+# For example,
+# Given input array nums = [1,1,2],
+# Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+
+def remove_duplicates(nums)
+  return 0 if nums.empty? 
+  
+  slow = 0
+  fast = 1
+
+  while fast < nums.length
+    if nums[slow] != nums[fast]
+      slow += 1
+      nums[slow] = nums[fast]
+    end
+
+    fast += 1
+  end
+
+  slow + 1
+end
 
 
 
