0978ff6af63ffd692bdc0eb23356737165bd80fc
Raymond Lee <raylee0525@gmail.com>
2017-08-09 11:13:19 -0700

    fb - 33

--- Algorithms/LeetCode/List.rb
+++ Algorithms/LeetCode/List.rb
@@ -532,6 +532,49 @@ def str_str(string, pattern)
   -1
 end
 
+# 33) Search in Rotated Sorted Array
+# Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+# (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+# You are given a target value to search. If found in the array return its index, otherwise return -1.
+# You may assume no duplicate exists in the array.
+
+# Brute force O(n)
+def search(nums, target)
+  nums.each_with_index do |num, idx|
+    return idx if target == num
+  end
+
+  -1
+end
+
+# Binary Search O(log n)
+def search(nums, target)
+  low, high = 0, nums.length - 1
+
+  while low <= high
+    mid = (low + high) / 2
+    return mid if nums[mid] == target
+
+    if nums[low] <= nums[mid]
+      if nums[low] <= target && target <= nums[mid]
+        high = mid - 1
+      else
+        low = mid + 1
+      end
+    else
+      if nums[mid] <= target && target <= nums[high]
+        low = mid + 1
+      else
+        high = mid - 1
+      end
+    end
+  end
+
+  -1
+end
+
+
+
 
 
 
