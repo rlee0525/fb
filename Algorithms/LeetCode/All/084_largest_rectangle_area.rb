56ce8a43eb82d8861c5be433af643572b577e0f8
Raymond Lee <raylee0525@gmail.com>
2017-08-16 11:03:50 -0700

    all - 084 linear

--- Algorithms/LeetCode/All/084_largest_rectangle_area.rb
+++ Algorithms/LeetCode/All/084_largest_rectangle_area.rb
@@ -22,3 +22,52 @@ def largest_rectangle_area(heights)
 
   max_rect
 end
+
+# O(N) - increasing stack data structure
+def largest_rectangle_area(heights)
+  stack = []
+  max_area = 0
+  i = 0
+
+  while i < heights.length
+    if stack.empty? || heights[i] >= heights[stack[-1]]
+      stack << i
+    else
+      shortest = stack.pop
+      left = stack.empty? ? 0 : stack[-1] + 1
+      max_area = [max_area, heights[shortest] * (i - left)].max
+      i -= 1
+    end
+
+    i += 1
+  end
+
+  current = heights.length
+  while !stack.empty?
+    shortest = stack.pop
+    left = stack.empty? ? 0 : stack[-1] + 1
+    max_area = [max_area, heights[shortest] * (current - left)].max
+  end
+
+  max_area
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
