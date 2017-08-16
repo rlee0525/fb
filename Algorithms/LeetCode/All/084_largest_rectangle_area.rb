9e38dff6557266f584481f6a7f2989b6dc0bf0f5
Raymond Lee <raylee0525@gmail.com>
2017-08-16 14:38:02 -0700

    all - 84 comments

--- Algorithms/LeetCode/All/084_largest_rectangle_area.rb
+++ Algorithms/LeetCode/All/084_largest_rectangle_area.rb
@@ -25,32 +25,42 @@ end
 
 # O(N) - increasing stack data structure
 def largest_rectangle_area(heights)
+  # Maintain increasing stack
   stack = []
   max_area = 0
   i = 0
 
+  # Iterate once (Increasing order)
   while i < heights.length
+    # If stack is empty or height is higher, push it in the stack
     if stack.empty? || heights[i] >= heights[stack[-1]]
       stack << i
     else
+      # if height is shorter than tallest one so far, find the max area of things taller than itself first
       shortest = stack.pop
+      # left bound, i = right bound
       left = stack.empty? ? 0 : stack[-1] + 1
       max_area = [max_area, heights[shortest] * (i - left)].max
+      # Keep repeating until we get rid of all the heights taller than itself.
       i -= 1
     end
 
     i += 1
   end
 
+  # Iterate again with the shorter ones
+  # right bound is the rightmost part (current)
   current = heights.length
   while !stack.empty?
     shortest = stack.pop
+    
     left = stack.empty? ? 0 : stack[-1] + 1
     max_area = [max_area, heights[shortest] * (current - left)].max
   end
 
   max_area
 end
+
 
 
 
